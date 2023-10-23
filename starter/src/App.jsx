bfd158f77dc7693ad759566929f55f519a8b90c3
LooneyCodesJr <148794380+LooneyCodesJr@users.noreply.github.com>
2023-10-23 18:32:29 +0100

    Add starter files via upload

---
+++ starter/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[url('/landing-bkg.jpg')] bg-cover bg-center">
+      <h1 className="text-5xl font-display text-white">Penguins of Antarctica</h1>
+    </div>
+  );
+}
+
+export default App;
